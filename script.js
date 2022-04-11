5797258e14ba2125fbd259b2a6eb5a1fe655847b
Flare <jetcadwallader@gmail.com>
2022-04-11 16:17:50 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function startTime() {
+    const today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth()+1;
+    let date = today.getDate();
+    let hour = today.getHours();
+    let minute = today.getMinutes();
+    let second = today.getSeconds();
+    minute = checkTime(minute);
+    second = checkTime(second);
+    document.getElementById('txt').innerHTML =  date + "/" + month + "/" + year + " - " + hour + ":" + minute + ":" + second;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
