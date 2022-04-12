5d7332e06418879133d23361ede2465ee6e60df5
Flare <jetcadwallader@gmail.com>
2022-04-12 16:47:16 +1200

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,21 @@ function startTime() {
     let minute = rtc.getMinutes();
     let second = rtc.getSeconds();
     let millisecond = rtc.getMilliseconds();
-    minute = checkTime(minute);
-    second = checkTime(second);
+    minute = checkTime00(minute);
+    second = checkTime00(second);
+    millisecond = checkTime000(millisecond);
     document.getElementById('txt').innerHTML =  date + "/" + month + "/" + year + " - " + hour + ":" + minute + ":" + second + ":" + millisecond;
     setTimeout(startTime, 1);
   }
   
-  function checkTime(i) {
+  function checkTime00(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
   }
+
+function checkTime000(i) {
+  if (i < 10) {i = "00" + i}
+  else if (i < 100 && i > 10) {i = "0" + i}
+  else {}
+  return i;
+} 
